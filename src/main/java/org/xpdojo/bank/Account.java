9c372b7011c5062a53029e44bce856a52678dcf7
Alua Beisekulova <abeisek@ncsu.edu>
2021-09-01 07:44:50 -0400

    initial

--- src/main/java/org/xpdojo/bank/Account.java
+++ src/main/java/org/xpdojo/bank/Account.java
@@ -1,4 +1,13 @@
 package org.xpdojo.bank;
 
 public class Account {
+    public int getBalance() {
+        return balance;
+    }
+
+    private int balance;
+
+    public void deposit(int amount) {
+        balance = balance+amount;
+    }
 }
--- src/test/java/org/xpdojo/bank/AccountTest.java
+++ src/test/java/org/xpdojo/bank/AccountTest.java
@@ -4,12 +4,16 @@ import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
-
+import static org.hamcrest.Matchers.*;
+import static org.hamcrest.MatcherAssert.assertThat;
 public class AccountTest {
 
     @Test
-	@Disabled
+
     public void depositAnAmountToIncreaseTheBalance() {
-        assertThat("your first test").isBlank();
+        Account account = new Account();
+        account.deposit(100);
+        assertThat(account.getBalance(), is(100));
     }
+
 }
